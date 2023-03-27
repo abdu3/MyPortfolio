0c814ca1e5bb422d60aec7beaa4d893e0827544a
عبدالرزاق عبدالفتاح الحمري <alhamri00357@gmail.com>
2023-03-27 18:09:28 +0300

    start skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Skills() {
+  return (
+        <section className="skills pt-5 bg-dark text-center">
+            <div className='title-skills'>
+                <h2 className='title'>Skills</h2>
+            </div>
+        </section>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Services from '../components/Services'
+import Skills from '../components/Skills'
 
 export default function Home() {
   return (
     <Fragment>
         <Header/>
         <Services/>
+        <Skills/>
     </Fragment>
   )
 }
